38b9135911a87c71bab23a3ccfdac0944cd2261c
e.lascano <emilas5678@gmail.com>
2023-02-26 19:58:09 -0500

    fix profile model

--- models/profile.js
+++ models/profile.js
@@ -5,7 +5,15 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
-  armies: [{type: mongoose.Schema.Types.ObjectId, ref: "Army"}]
+  armies: [{type: mongoose.Schema.Types.ObjectId, ref: "Army"}],
+  loveTypes: [{
+    type: String,
+    enum: ["Eros", "Philia", "Storge", "Agape", "Ludus", "Pragma", "Philautia", "Mania"],
+  }],
+  loveLanguages: [{
+    type: String,
+    enum: ["Words of Affirmation", "Acts of Service", "Receiving Gifts", "Quality Time", "Physical Touch"],
+  }],
 },{
   timestamps: true,
 })
