f3ca9c47bcc6ddda2c3463222987354058d21d5e
e.lascano <emilas5678@gmail.com>
2023-03-05 00:38:25 -0500

    fix profile show controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -32,6 +32,11 @@ function addPhoto(req, res) {
 function show (req, res) {
   console.log(req.user.profile)
   Profile.findById(req.user.profile)
+  .populate({
+      path: 'plans',
+      populate: { path: 'who', select: "name image"}
+    })
+  // .populate("plans")
   .then(myProfile => {
     res.json(myProfile)
   })
